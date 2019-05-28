6c8064944bd11743458e7bb8a4dd42dd9da86528
WUAN <291829614@qq.com>
2019-05-28 21:31:45 +0800

    fix

--- main/main.js
+++ main/main.js
@@ -1,4 +1,18 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    let price = 0;
+	if(inputs.distance < 2)
+	{
+		price = 6 + 0.25*inputs.parkTime;
+	}
+	else if(inputs.distance <= 8)
+	{
+		price = 6 + 0.8*(inputs.distance - 2) + 0.25*inputs.parkTime;
+	}
+	else
+	{
+		price = 6 + 1.2*(inputs.distance - 2) + 0.25*inputs.parkTime;
+	}
+	price = Math.round(price); 
+	return price;
 };
