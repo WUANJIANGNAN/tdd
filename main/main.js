8c2a831688b48d822492d25be9a3d85520a686b9
WUANJIANGNAN <51110086+WUANJIANGNAN@users.noreply.github.com>
2019-05-30 19:42:08 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -2,8 +2,9 @@ module.exports = function main(inputs) {
     // write your code here...
 	let price = 6 + 0.25*inputs.parkTime;
 	if(inputs.distance > 2 && inputs.distance <= 8)
-		price = Math.round(price += 0.8*(inputs.distance - 2));
+		price += 0.8*(inputs.distance - 2);
 	else if(inputs.distance > 8)
-		price = Math.round(price += 4.8 + 1.2*(inputs.distance - 8));
+		price += 4.8 + 1.2*(inputs.distance - 8);
+	price = Math.round(price);
 	return price;
 };
